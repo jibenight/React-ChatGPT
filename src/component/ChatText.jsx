3bef239b953b2d1f31d7acbf9fd896ec6a58c7ab
Jean <nguyen.jean@me.com>
2023-03-25 09:10:29 +0100

    Chatzone componen

---
+++ src/component/ChatText.jsx
@@ -0,0 +1,17 @@
+function ChatText(props) {
+  return (
+    <div className='rounded-lg m-2 p-3 bg-slate-200'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni aperiam
+        quod voluptatem ipsa totam laudantium ducimus nesciunt velit enim nulla
+        provident, autem vitae? Libero itaque exercitationem adipisci dolore
+        fuga dolores?Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatum, ut harum eos architecto perspiciatis pariatur, alias et
+        eligendi esse sapiente voluptatibus quidem deleniti sint ducimus nobis
+        nisi dolorem minima molestias.
+      </p>
+    </div>
+  );
+}
+
+export default ChatText;
--- src/component/ChatZone.jsx
+++ src/component/ChatZone.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import ChatInput from './ChatInput';
+import ChatText from './ChatText';
 
 function ChatZone() {
   const [messages, setMessages] = useState([]);
 
   return (
     <div className='relative bg-gray-100 h-screen'>
+      <ChatText />
       <ChatInput />
     </div>
   );

779310e77d4566f786750343c2193e7271459a5f
Jean <nguyen.jean@me.com>
2023-03-26 20:58:36 +0200

    add assets

--- src/component/Aside.jsx
+++ src/component/Aside.jsx
@@ -3,8 +3,12 @@ import Aioption from './Aioption';
 
 function Aside() {
   return (
-    <div className='bg-gray-800 w-100 h-screen'>
-      <div className='m-2'>
+    <div className='bg-gray-800 w-100 h-screen divide-y divide-gray-100'>
+      <div className='m-3 h-10'>
+        <h1 className='text-gray-100 text-2xl italic'>Hello, Jean</h1>
+      </div>
+
+      <div className='m-3'>
         <Aioption />
       </div>
     </div>
